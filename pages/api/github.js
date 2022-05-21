9b8e50c2d5e847cef8109048ca777f40b80783f2
Yuwan Samega <yuwansamega28@gmail.com>
2022-05-21 18:39:03 +0700

    api, display data

---
+++ pages/api/github.js
@@ -0,0 +1,24 @@
+import { Octokit } from "@octokit/rest";
+
+export default async (req, res) => {
+  
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_AUTH_TOKEN
+  });
+
+  const repos = await octokit.request("/users/yuwansamega/repos");
+
+  const dataRepos = repos.data;
+
+  const reposCount = repos.data.length;
+
+  const starsCount = repos.data.filter(repo => !repo.fork).reduce((acc, item) => {
+    return acc + item.stargazers_count;
+  },0);
+
+  const starred = await octokit.request("/users/yuwansamega/starred");
+
+  const starredCount = starred.data.length;
+
+  return res.status(200).json({dataRepos: dataRepos, reposCount: reposCount, starsCount: starsCount, starredCount: starredCount});
+}
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,70 @@
 import Head from 'next/head'
 import React from 'react'
+import useSWR from 'swr'
+
+
+async function fetcher(...arg){
+  const res = await fetch(...arg);
+
+  return res.json();
+}
 
 const Home = () => {
+  
+  const { data, error } = useSWR('api/github', fetcher);
+  if (error) {
+    console.log(error)
+  }
+
   return (
     <>
       <Head>
         <title>
-          Repository List
+          Telkom Frontend Task
         </title>
       </Head>
 
-      <div className='h-screen w-full flex justify-center bg-gray-900' >
+      <div className='h-fit w-full flex justify-center bg-[#121417] pb-24' >
         <div className='flex flex-col w-[70%] items-center'>
           <div className='text-slate-100 text-2xl font-medium mt-12'>
-            <h1>Yuwan's Repository</h1>
+            <h1>Yuwan's Github Dashboard</h1>
           </div>
-          <div className='w-full border-b border-slate-300 mt-5'></div>
-          <div className='flex gap-y-5 flex-col items-center w-full mt-10'>
-            <div className='p-5 text-white w-full h-32 border border-slate-300 rounded-lg tex-white'>
-              <h1>Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti facere quod eos?</p>
+          <div className='flex flex-row gap-x-5 items-center justify-center w-full mt-10'>
+            <div className='flex flex-col justify-center p-3 gap-y-2 h-24 w-52 border-2 border-cyan-300 rounded-md text-slate-300'>
+              <h1 className='font-medium text-slate-200'>Github Stars</h1>
+              <p>{data?.starsCount}</p>
             </div>
-            <div className='p-5 text-white w-full h-32 border border-slate-300 rounded-lg tex-white'>
-              <h1>Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti facere quod eos?</p>
+            <div className='flex flex-col justify-center p-3 gap-y-2 h-24 w-52 border-2 border-purple-400 rounded-md text-slate-300'>
+              <h1 className='font-medium text-slate-200'>Github Repos</h1>
+              <p>{data?.reposCount}</p>
             </div>
+            <div className='flex flex-col justify-center p-3 gap-y-2 h-24 w-52 border-2 border-amber-300 rounded-md text-slate-300'>
+              <h1 className='font-medium text-slate-200'>Repos Starred</h1>
+              <p>{data?.starredCount}</p>
+            </div>
+            
+          </div>
+          <div className='w-full border-b border-slate-300 mt-10'></div>
+          <div className='flex flex-col gap-y-5 items-center w-full mt-10'>
+
+          {data ? data.dataRepos.map(item => {
+                    return (
+                      <>
+                        <div key={item.id} className='flex flex-col justify-between p-5 w-[90%] h-32 border-2 border-gray-300 rounded-md hover:bg-gray-500 hover:bg-opacity-40'>
+                          <a href={item.svn_url} className="cursor-pointer hover:underline hover:underline-offset-1 hover:decoration-pink-500" target='_blank'>
+                            <h1 className='text-slate-100 text-xl font-medium'>{item.name}</h1>
+                          </a>
+                          <p className='font-normal text-sm text-gray-300'>{item.created_at.slice(0, 10)}</p>
+                        </div>
+                      </>
+                    )
+                  }) : "loading"}
           </div>
         </div>
+      </div>
+
+      <div className='w-full bg-[#393E46] pt-8 pb-8 flex justify-center'>
+          <h1 className='text-white text-base'><span className='text-pink-600 font-medium'>Yuwan Samega</span> made with ❤️ &copy;2022 | All Right Reserved. </h1>
       </div>  
       
       
